1b04c3bf401d135180eaf7d41481103973137ea9
Maytee Akkaramongkolrojn <77748481+MayteeAkk@users.noreply.github.com>
2022-03-23 16:39:24 -0400

    Made Some Progress on the longestSubstring Problem

--- Sliding Window/longestSubstringWithKDistinctChars.js
+++ Sliding Window/longestSubstringWithKDistinctChars.js
@@ -1,3 +1,17 @@
-function longestSubstring(){
+function longestSubstring(str, k){
+    let letterMap = {};
+    let maxLength = 0;
+    let windowStart = 0;
 
+    for(let windowEnd  = 0; windowEnd < str.length; windowEnd++){
+        let rightChar = str[windowEnd];
+        //If Char is not already in HashMap, add it
+        if(!(rightChar in letterMap)){
+            letterMap[rightChar] = 1;
+        }
+        letterMap[rightChar] += 1;
+
+        
+
+    }
 } 
